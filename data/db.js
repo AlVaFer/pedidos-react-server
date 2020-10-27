0ea018fe6bfb0e9a7723922ce5f3d26f6cd9234a
Al Tfe <heroku7@gmx.es>
2020-10-27 13:45:07 +0000

    commit actualizar

---
+++ data/db.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/clientes', {userNewUrlParser: true});
+
+
+// Definir el Schema de clientes
+const clientesSchema = new mongoose.Schema({
+    nombre: String,
+    apellido: String,
+    empresa: String,
+    emails: Array,
+    edad: Number,
+    tipo: String,
+    pedidos: Array
+})
+const Clientes = mongoose.model('clientes', clientesSchema);
+
+// Productos
+const productosSchema = new mongoose.Schema({
+    nombre: String,
+    precio: Number,
+    stock: Number
+})
+const Productos = mongoose.model('productos', productosSchema);
+
+// Pedidos
+const pedidosSchema = new mongoose.Schema({
+    pedido: Array,
+    total: Number,
+    fecha: Date,
+    cliente: mongoose.Types.ObjectId,
+    estado: String
+})
+const Pedidos = mongoose.model('pedidos', pedidosSchema);
+
+export { Clientes, Productos, Pedidos };
